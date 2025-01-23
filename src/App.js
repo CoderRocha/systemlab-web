226a5ad2e1eaa8bfb230b30d65f90dec0f7e3414
Guilherme <guilhermerochas0420@gmail.com>
2025-01-22 23:07:30 -0300

    added basic login logic (front)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Login from './pages/login/Login'
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <div className="App">
-
-      <Login />
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // styles
 import styles from './Login.module.css'
 import Logo from '../../assets/Systemlab-logo.png'
 
-export default function Login() {
+function Login() {
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        navigate('/home');
+    }
 
-  return (
-    <form onSubmit={handleSubmit} className={styles['login-form']}>
-        <img src={Logo} alt='SystemLab Web Logo' />
-      <h2>Bem vindo!</h2>
-      <label>
-        <span>Usuário</span>
-        <input
-        type="text"
-        />
-      </label>
-      <label>
-        <span>Senha</span>
-        <input
-        type="password"
-        />
-      </label>
-      <button className="btn">Entrar</button>
-    </form>
-  )
+    return (
+        <form onSubmit={handleSubmit} className={styles['login-form']}>
+            <img src={Logo} alt='SystemLab Web Logo' />
+            <h2>Bem vindo!</h2>
+            <label>
+                <span>Usuário</span>
+                <input
+                    type="text"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                />
+            </label>
+            <label>
+                <span>Senha</span>
+                <input
+                    type="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                />
+            </label>
+            <button className="btn">Entrar</button>
+        </form>
+    )
 }
+
+export default Login;
