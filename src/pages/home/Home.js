e356f7ec8f376b73de9a03fb0b2e5a9c009d7476
Guilherme <guilhermerochas0420@gmail.com>
2025-01-23 22:43:28 -0300

    removed unwanted code

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
 //styles
 import styles from './Home.module.css';
 import Navbar from '../../components/navbar/Navbar';
 
 const Home = () => {
-    const navigate = useNavigate();
-
-    const handleClick = () => {
-        navigate('/home');
-    };
 
     return (
         <>
