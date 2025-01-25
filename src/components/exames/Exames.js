f589bee7d8b10fcfa7672e583c64dc7c4468cfda
Guilherme <guilhermerochas0420@gmail.com>
2025-01-24 21:40:36 -0300

    updated 'Exames' code and styles

--- src/components/exames/Exames.js
+++ src/components/exames/Exames.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 //styles
-import styles from './Exames.module.css'
+import styles from './Exames.module.css';
 
 import Navbar from '../../components/navbar/Navbar';
 
 export default function Exames() {
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  const handleClick = () => {
+    navigate('/cadastrarexame');
+  };
+
   return (
     <>
-    <Navbar />
-    <div>Exames</div>
+      <Navbar />
+      <div className={styles.container}>
+        <button className={styles['btn']} onClick={handleClick}>Criar Novo Exame</button>
+        <div className={styles.listContainer}>
+          <h2>Exames Cadastrados</h2>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>Código</th>
+                <th>Descrição</th>
+                <th>Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>HEMO</td>
+                <td>Hemograma</td>
+                <td>R$ 120,00</td>
+                <td>Acessar</td>
+              </tr>
+              <tr>
+                <td>URE</td>
+                <td>Ureia</td>
+                <td>R$ 250,00</td>
+                <td>Acessar</td>
+              </tr>
+              <tr>
+                <td>GLID</td>
+                <td>Glicose</td>
+                <td>R$ 180,00</td>
+                <td>Acessar</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
-  )
+  );
 }
