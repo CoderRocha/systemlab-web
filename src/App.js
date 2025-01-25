dd2996c39a85c38a6ab27955d783144b8e87a764
Guilherme <guilhermerochas0420@gmail.com>
2025-01-24 22:02:40 -0300

    added the 'Footer' to its own component

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Exames from './components/exames/Exames.js';
 import Relatorios from './pages/relatorios/Relatorios.js';
 import CadastrarAtendimento from './pages/cadastrarAtendimento/CadastrarAtendimento.js'
 import CadastrarExame from './pages/cadastrarExame/CadastrarExame.js';
+import Footer from './components/footer/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/atendimentos" element={<Atendimentos />} />
-          <Route path="/exames" element={<Exames />} />
-          <Route path="/relatorios" element={<Relatorios />} />
-          <Route path="/cadastraratendimento" element={<CadastrarAtendimento />} />
-          <Route path="/cadastrarexame" element={<CadastrarExame />} />
-        </Routes>
-      </Router>
-      <footer>
-        <p>©2025 SystemLab Web</p>
-      </footer>
-    </div>
-
+    <>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/atendimentos" element={<Atendimentos />} />
+            <Route path="/exames" element={<Exames />} />
+            <Route path="/relatorios" element={<Relatorios />} />
+            <Route path="/cadastraratendimento" element={<CadastrarAtendimento />} />
+            <Route path="/cadastrarexame" element={<CadastrarExame />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//styles
+import styles from './Footer.module.css'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer>
+                <p>©2025 SystemLab Web</p>
+            </footer>
+        </div>
+    )
+}
