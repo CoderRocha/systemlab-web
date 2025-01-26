51092ec90fd8ef609bea976937a346eb656cfbc9
Guilherme <guilhermerochas0420@gmail.com>
2025-01-26 18:06:32 -0300

    created 'database.js' for the sqlite and basic 'server.js' logic

---
+++ back-end/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = require('./database');
+
+const app = express();
+
+// middleman
+app.use(cors());
+app.use(bodyParser.json());
+
+// Endpoints bem básicos
+app.get('/', (req, res) => {
+    res.send('API do Sistema de Pacientes está funcionando!');
+});
+
+// outros endpoints estarão aqui
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
