877e6768abd4dc706b04640ca7310062b373bab3
Guilherme <guilhermerochas0420@gmail.com>
2025-01-28 19:25:56 -0300

    added 'react-icons' to the 'navbar' component

--- front-end/src/components/navbar/Navbar.js
+++ front-end/src/components/navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-
-//styles
+import { FaClipboardList, FaFlask, FaChartBar, FaSignOutAlt } from 'react-icons/fa'; // Importando ícones
 import styles from './Navbar.module.css';
 import Logo from '../../assets/Systemlab-logo.png';
 
@@ -14,48 +13,32 @@ const Navbar = () => {
 
     return (
         <div className={styles['navbar-container']}>
-            {/* Logo with navigation to home */}
+            {/* Logo */}
             <img
                 src={Logo}
                 alt="SystemLab Web Logo"
                 className={styles['navbar-logo']}
                 onClick={() => handleNavigate('/home')}
             />
-            
-            {/* Navigation Section */}
+
+            {/* Navegação */}
             <nav className={styles['navbar-nav']}>
                 <ul className={styles['navbar-list']}>
-                    <li className={styles['navbar-item']}>
-                        <span
-                            className={styles['navbar-link']}
-                            onClick={() => handleNavigate('/atendimentos')}
-                        >
-                            Atendimentos
-                        </span>
+                    <li className={styles['navbar-item']} onClick={() => handleNavigate('/atendimentos')}>
+                        <FaClipboardList className={styles['navbar-icon']} />
+                        <span className={styles['navbar-link']}>Atendimentos</span>
                     </li>
-                    <li className={styles['navbar-item']}>
-                        <span
-                            className={styles['navbar-link']}
-                            onClick={() => handleNavigate('/exames')}
-                        >
-                            Exames
-                        </span>
+                    <li className={styles['navbar-item']} onClick={() => handleNavigate('/exames')}>
+                        <FaFlask className={styles['navbar-icon']} />
+                        <span className={styles['navbar-link']}>Exames</span>
                     </li>
-                    <li className={styles['navbar-item']}>
-                        <span
-                            className={styles['navbar-link']}
-                            onClick={() => handleNavigate('/relatorios')}
-                        >
-                            Relatórios
-                        </span>
+                    <li className={styles['navbar-item']} onClick={() => handleNavigate('/relatorios')}>
+                        <FaChartBar className={styles['navbar-icon']} />
+                        <span className={styles['navbar-link']}>Relatórios</span>
                     </li>
-                    <li className={styles['navbar-item']}>
-                        <span
-                            className={styles['navbar-link']}
-                            onClick={() => handleNavigate('/')}
-                        >
-                            Sair
-                        </span>
+                    <li className={styles['navbar-item']} onClick={() => handleNavigate('/')}>
+                        <FaSignOutAlt className={styles['navbar-icon']} />
+                        <span className={styles['navbar-link']}>Sair</span>
                     </li>
                 </ul>
             </nav>
