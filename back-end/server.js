eb65cafd6124091382a8208e521a004b146dbee4
Guilherme <guilhermerochas0420@gmail.com>
2025-01-29 01:17:26 -0300

    fixed 'server.js' code layout

--- back-end/server.js
+++ back-end/server.js
@@ -11,58 +11,58 @@ app.use(bodyParser.json());
 
 // endpoint de teste
 app.get('/', (req, res) => {
-    res.send('API do Sistema está funcionando!');
+  res.send('API do Sistema está funcionando!');
 });
 
 // endpoint para cadastrar um atendimento
 app.post('/atendimentos', (req, res) => {
-    const { numeroAtendimento, nomePaciente, sexo, email, celular, exames } = req.body;
+  const { numeroAtendimento, nomePaciente, sexo, email, celular, exames } = req.body;
 
-    const sqlAtendimento = `
+  const sqlAtendimento = `
         INSERT INTO atendimentos (numero_atendimento, nome_completo, sexo, email, celular)
         VALUES (?, ?, ?, ?, ?)
     `;
-    const paramsAtendimento = [numeroAtendimento, nomePaciente, sexo, email, celular];
+  const paramsAtendimento = [numeroAtendimento, nomePaciente, sexo, email, celular];
 
-    db.run(sqlAtendimento, paramsAtendimento, function (err) {
-        if (err) {
-            console.error('Erro ao salvar atendimento:', err.message);
-            return res.status(500).json({ message: 'Erro ao salvar atendimento.' });
-        }
+  db.run(sqlAtendimento, paramsAtendimento, function (err) {
+    if (err) {
+      console.error('Erro ao salvar atendimento:', err.message);
+      return res.status(500).json({ message: 'Erro ao salvar atendimento.' });
+    }
 
-        const atendimentoId = this.lastID;
+    const atendimentoId = this.lastID;
 
-        // inserir os exames associados ao atendimento (se houver)
-        if (exames && exames.length > 0) {
-            const sqlExames = `
+    // inserir os exames associados ao atendimento (se houver)
+    if (exames && exames.length > 0) {
+      const sqlExames = `
                 INSERT INTO paciente_exames (paciente_id, exame_id)
                 SELECT ?, id FROM exames WHERE codigo = ?
             `;
 
-            const examesPromises = exames.map((codigoExame) => {
-                return new Promise((resolve, reject) => {
-                    db.run(sqlExames, [atendimentoId, codigoExame], (err) => {
-                        if (err) reject(err);
-                        else resolve();
-                    });
-                });
-            });
+      const examesPromises = exames.map((codigoExame) => {
+        return new Promise((resolve, reject) => {
+          db.run(sqlExames, [atendimentoId, codigoExame], (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
+      });
 
-            Promise.all(examesPromises)
-                .then(() => res.status(201).json({ message: 'Atendimento salvo com sucesso!' }))
-                .catch((error) => {
-                    console.error('Erro ao salvar exames:', error.message);
-                    res.status(500).json({ message: 'Erro ao salvar exames do atendimento.' });
-                });
-        } else {
-            res.status(201).json({ message: 'Atendimento salvo com sucesso!' });
-        }
-    });
+      Promise.all(examesPromises)
+        .then(() => res.status(201).json({ message: 'Atendimento salvo com sucesso!' }))
+        .catch((error) => {
+          console.error('Erro ao salvar exames:', error.message);
+          res.status(500).json({ message: 'Erro ao salvar exames do atendimento.' });
+        });
+    } else {
+      res.status(201).json({ message: 'Atendimento salvo com sucesso!' });
+    }
+  });
 });
 
 // endpoint para listar todos os atendimentos cadastrados
 app.get('/atendimentos', (req, res) => {
-    const sql = `
+  const sql = `
         SELECT 
             a.numero_atendimento AS numero_atendimento, 
             a.nome_completo AS nomePaciente, 
@@ -75,15 +75,15 @@ app.get('/atendimentos', (req, res) => {
         LEFT JOIN exames e ON pe.exame_id = e.id
         GROUP BY a.id
     `;
-  
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            console.error('Erro ao buscar atendimentos:', err.message);
-            return res.status(500).json({ message: 'Erro ao buscar atendimentos.' });
-        }
-  
-        res.status(200).json(rows);
-    });
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error('Erro ao buscar atendimentos:', err.message);
+      return res.status(500).json({ message: 'Erro ao buscar atendimentos.' });
+    }
+
+    res.status(200).json(rows);
+  });
 });
 
 // endpoint para deletar um atendimento
@@ -93,7 +93,7 @@ app.delete('/atendimentos/:numero_atendimento', (req, res) => {
   const sqlDeleteExames = `
     DELETE FROM paciente_exames WHERE paciente_id = (SELECT id FROM atendimentos WHERE numero_atendimento = ?)
   `;
-  
+
   const sqlDeleteAtendimento = `
     DELETE FROM atendimentos WHERE numero_atendimento = ?
   `;
@@ -119,98 +119,98 @@ app.delete('/atendimentos/:numero_atendimento', (req, res) => {
 
 // endpoint para cadastrar um exame
 app.post('/exames', (req, res) => {
-    const { codigo, descricao, valor } = req.body;
+  const { codigo, descricao, valor } = req.body;
 
-    // Verificar se o código do exame já existe, ignorando a diferença de maiúsculas/minúsculas
-    const sqlCheckExame = `SELECT id FROM exames WHERE LOWER(codigo) = LOWER(?)`;
-    
-    db.get(sqlCheckExame, [codigo], (err, row) => {
-        if (err) {
-            console.error('Erro ao verificar exame existente:', err.message);
-            return res.status(500).json({ message: 'Erro ao verificar exame.' });
-        }
+  // verificar se o código do exame já existe, ignorando a diferença de lower e upper case (case-sensitive)
+  const sqlCheckExame = `SELECT id FROM exames WHERE LOWER(codigo) = LOWER(?)`;
 
-        if (row) {
-            // Se o exame já existe, retornamos uma mensagem de erro
-            return res.status(400).json({ message: 'Exame com esse código já cadastrado.' });
-        }
+  db.get(sqlCheckExame, [codigo], (err, row) => {
+    if (err) {
+      console.error('Erro ao verificar exame existente:', err.message);
+      return res.status(500).json({ message: 'Erro ao verificar exame.' });
+    }
 
-        // Caso não exista, insere o novo exame
-        const sql = `
+    if (row) {
+      // se o exame já existe, retorne uma mensagem de erro
+      return res.status(400).json({ message: 'Exame com esse código já cadastrado.' });
+    }
+
+    // caso não exista, cadastre o novo exame
+    const sql = `
             INSERT INTO exames (codigo, descricao, valor)
             VALUES (?, ?, ?)
         `;
-        const params = [codigo, descricao, valor];
+    const params = [codigo, descricao, valor];
 
-        db.run(sql, params, function (err) {
-            if (err) {
-                console.error('Erro ao salvar exame:', err.message);
-                return res.status(500).json({ message: 'Erro ao salvar exame.' });
-            }
+    db.run(sql, params, function (err) {
+      if (err) {
+        console.error('Erro ao salvar exame:', err.message);
+        return res.status(500).json({ message: 'Erro ao salvar exame.' });
+      }
 
-            res.status(201).json({ message: 'Exame cadastrado com sucesso!', exameId: this.lastID });
-        });
+      res.status(201).json({ message: 'Exame cadastrado com sucesso!', exameId: this.lastID });
     });
+  });
 });
 
 // endpoint para deletar um exame
 app.delete('/exames/:codigo', (req, res) => {
-    const { codigo } = req.params;
-  
-    // Deletar o exame
-    const sqlDeleteExame = `DELETE FROM exames WHERE codigo = ?`;
-  
-    db.run(sqlDeleteExame, [codigo], function (err) {
-      if (err) {
-        console.error('Erro ao deletar exame:', err.message);
-        return res.status(500).json({ message: 'Erro ao deletar exame.' });
-      }
-  
-      if (this.changes === 0) {
-        return res.status(404).json({ message: 'Exame não encontrado.' });
-      }
-  
-      res.status(200).json({ message: 'Exame deletado com sucesso!' });
-    });
-  });  
+  const { codigo } = req.params;
+
+  // deletar o exame
+  const sqlDeleteExame = `DELETE FROM exames WHERE codigo = ?`;
+
+  db.run(sqlDeleteExame, [codigo], function (err) {
+    if (err) {
+      console.error('Erro ao deletar exame:', err.message);
+      return res.status(500).json({ message: 'Erro ao deletar exame.' });
+    }
+
+    if (this.changes === 0) {
+      return res.status(404).json({ message: 'Exame não encontrado.' });
+    }
+
+    res.status(200).json({ message: 'Exame deletado com sucesso!' });
+  });
+});
 
 // endpoint para listar todos os exames cadastrados
 app.get('/exames', (req, res) => {
-    const sql = `SELECT codigo, descricao, valor FROM exames`;
+  const sql = `SELECT codigo, descricao, valor FROM exames`;
 
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            console.error('Erro ao buscar exames:', err.message);
-            return res.status(500).json({ message: 'Erro ao buscar exames.' });
-        }
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error('Erro ao buscar exames:', err.message);
+      return res.status(500).json({ message: 'Erro ao buscar exames.' });
+    }
 
-        res.status(200).json(rows);
-    });
+    res.status(200).json(rows);
+  });
 });
 
-// Endpoint para verificar a existência de um exame pelo código
+// endpoint para verificar a existência de um exame pelo código
 app.get('/exames/:codigo', (req, res) => {
-    const { codigo } = req.params;
-    
-    const sqlCheckExame = `SELECT id FROM exames WHERE codigo = ?`;
-  
-    db.get(sqlCheckExame, [codigo], (err, row) => {
-      if (err) {
-        console.error('Erro ao verificar exame:', err.message);
-        return res.status(500).json({ message: 'Erro ao verificar exame.' });
-      }
-  
-      if (row) {
-        res.status(200).json({ message: 'Exame encontrado.' });
-      } else {
-        res.status(404).json({ message: 'Exame não encontrado.' });
-      }
-    });
-  });
+  const { codigo } = req.params;
 
-// endpoint para gerar o relatório de pacientes por data
+  const sqlCheckExame = `SELECT id FROM exames WHERE codigo = ?`;
+
+  db.get(sqlCheckExame, [codigo], (err, row) => {
+    if (err) {
+      console.error('Erro ao verificar exame:', err.message);
+      return res.status(500).json({ message: 'Erro ao verificar exame.' });
+    }
+
+    if (row) {
+      res.status(200).json({ message: 'Exame encontrado.' });
+    } else {
+      res.status(404).json({ message: 'Exame não encontrado.' });
+    }
+  });
+});
+
+// endpoint para gerar o relatório geral de atendimentos
 app.get('/relatorios', (req, res) => {
-    const sql = `
+  const sql = `
       SELECT 
         a.numero_atendimento AS codigo_paciente, 
         a.nome_completo, 
@@ -224,19 +224,19 @@ app.get('/relatorios', (req, res) => {
       LEFT JOIN exames e ON pe.exame_id = e.id
       GROUP BY a.id
     `;
-    
-    db.all(sql, [], (err, rows) => {
-      if (err) {
-        console.error('Erro ao buscar relatórios:', err.message);
-        return res.status(500).json({ message: 'Erro ao buscar relatórios.' });
-      }
-  
-      res.status(200).json(rows);
-    });
-  });   
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error('Erro ao buscar relatórios:', err.message);
+      return res.status(500).json({ message: 'Erro ao buscar relatórios.' });
+    }
+
+    res.status(200).json(rows);
+  });
+});
 
 // configuração do servidor local
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
